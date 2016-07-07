2541425b06dc65cabd4122b5b52f41a5ae51c966
Dxny boy <dany@delacruz.pe>
2016-07-06 22:34:46 -0500

    cambios 06 - 07 -2016
    link a facebook

--- index.php
+++ index.php
@@ -401,15 +401,15 @@ $var = 'hola';
                 </div>
                 <div class="col-md-4">
                     <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter"></i></a>
+                        <!--<li><a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>-->
+                        <li><a href="https://www.facebook.com/Gesti%C3%B3n-Soluciones-Integrales-523607891172617" target="_blank" ><i class="fa fa-facebook"></i></a>
                         </li>
-                        <li><a href="#"><i class="fa fa-facebook"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                        </li>
+                        <!--<li><a href="#"><i class="fa fa-linkedin"></i></a>
+                        </li>-->
                     </ul>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 hidden">
                     <ul class="list-inline quicklinks">
                         <li><a href="#">Privacy Policy</a>
                         </li>
